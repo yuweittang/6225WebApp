2e5c164268f25077b5b4ba26e006c85b83865e40
Yuwei Tang <tang.yuw@northeastern.edu>
2023-04-12 14:00:42 -0700

    test

--- aws-infra-main/module/networking/provider.tf
+++ aws-infra-main/module/networking/provider.tf
@@ -151,6 +151,12 @@ resource "aws_instance" "web" {
   }
     EOF
 }
+variable "public_key" {
+  type = string
+}
+variable "private_key" {
+  type = string
+}
 
 variable "get_ami" {
   type = string
