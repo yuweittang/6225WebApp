b4ffcdd39d8b53a62fbf28734e10fe53ffe32089
yuweittang <98113535+yuweittang@users.noreply.github.com>
2023-04-18 21:58:06 -0700

    modify port

--- aws-infra-main/module/networking/provider.tf
+++ aws-infra-main/module/networking/provider.tf
@@ -42,8 +42,8 @@ resource "aws_security_group" "web" {
   }
 
   ingress {
-    from_port   = 8081
-    to_port     = 8081
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
