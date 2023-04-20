37e7767ed6c7dffb913d3b8cc072e94be3b74ce8
yuweittang <98113535+yuweittang@users.noreply.github.com>
2023-04-20 15:11:58 -0700

    08

--- aws-infra-main/module/networking/provider.tf
+++ aws-infra-main/module/networking/provider.tf
@@ -15,7 +15,6 @@ variable "region" {
 output "region" {
   value = var.region
 }
-
 resource "aws_security_group" "web" {
   name   = "aws_security_group.web"
   vpc_id = aws_vpc.yvot.id
@@ -140,6 +139,51 @@ resource "aws_instance" "web" {
     Name = "my-instance"
   }
   }
+                echo "Downloading and installing CloudWatch Agent..."
+              wget https://s3.amazonaws.com/amazoncloudwatch-agent/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm
+              sudo rpm -U ./amazon-cloudwatch-agent.rpm
+              
+              echo "Creating CloudWatch Agent config file..."
+              cat > /opt/aws/amazon-cloudwatch-agent/bin/config.json <<-CONFIG
+              {
+                "agent": {
+                    "metrics_collection_interval": 10,
+                    "logfile": "/var/logs/amazon-cloudwatch-agent.log"
+                },
+                "logs": {
+                    "logs_collected": {
+                        "files": {
+                            "collect_list": [
+                                {
+                                    "file_path": "/var/log/tomcat9/csye6225.log",
+                                    "log_group_name": "csye6225",
+                                    "log_stream_name": "webapp"
+                                }
+                            ]
+                        }
+                    },
+                    "log_stream_name": "cloudwatch_log_stream"
+                },
+                "metrics":{
+                  "metrics_collected":{
+                     "statsd":{
+                        "service_address":":8125",
+                        "metrics_collection_interval":15,
+                        "metrics_aggregation_interval":300
+                     }
+                  }
+                 }
+              }
+              CONFIG
+              
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
+    -a fetch-config \
+    -m ec2 \
+    -c file:/opt/cloudwatch-config.json \
+    -s
+
+
+
     EOF
 }
 
